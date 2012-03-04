3eed08cb9c70352476d2173098a3cc8d3274c39d
pseudoh <hasqulq@gmail.com>
2012-03-04 14:37:10 +0000

    Removed Machine dependent unit

--- frmMain.pas
+++ frmMain.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, BSATypes, BSAUnpack, CodeSiteLogging;
+  Dialogs, StdCtrls, BSATypes, BSAUnpack;
 
 type
   TForm5 = class(TForm)
